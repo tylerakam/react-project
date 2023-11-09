03f704eadbeb5c661affa785b3cd5e772fedd572
TylerAkam <tza13@psu.edu>
2023-11-09 16:36:06 -0500

    added chiefofbricks gallery

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+import { useState } from 'react';
+import { thumbList } from './data.js';
+
+export default function Gallery() {
+    const [index, setIndex] = useState(0);
+    const [showMore, setShowMore] = useState(false);
+    const hasNext = index < thumbList.length - 1;
+
+    function handleNextClick() {
+        if (hasNext) {
+            setIndex(index + 1);
+        } else {
+            setIndex(0);
+        }
+    }
+
+    function handleMoreClick() {
+        setShowMore(!showMore);
+    }
+
+    let thumb = thumbList[index];
+    return (
+        <>
+            <h1>Recent Chiefofbricks Video Gallery</h1>
+            <button onClick={handleNextClick}>
+                Next
+            </button>
+            <h2>
+                <i>{thumb.name} </i>
+                by {thumb.artist}
+            </h2>
+            <h3>
+                ({index + 1} of {thumbList.length})
+            </h3>
+            <button onClick={handleMoreClick}>
+                {showMore ? 'Hide' : 'Show'} details
+            </button>
+            {showMore && <p>{thumb.description}</p>}
+            <img
+                src={thumb.url}
+                alt={thumb.alt}
+            />
+        </>
+    );
 }
 
-export default App;
+
---
+++ src/data.js
@@ -0,0 +1,50 @@
+export const thumbList = [{
+    name: 'Say this when you\'re in trouble...',
+    artist: 'Chiefofbricks',
+    description: 'Jack learns a very important lesson about how to get out of sticky situations. ',
+    url: 'https://i.ytimg.com/an_webp/mLw8_0jW9mM/mqdefault_6s.webp?du=3000&sqp=CKfitKoG&rs=AOn4CLAX7gyC0Nw2UUSm8-0JlJLmnhFQOg',
+    alt: 'Say this when you\'re in trouble... video thumbnail'
+}, {
+    name: 'it\'s a really strong prescription',
+    artist: 'Chiefofbricks',
+    description: 'Dylan tries on Tyler\'s new prescription',
+    url: 'https://i.ytimg.com/an_webp/rGLQr95xmz4/mqdefault_6s.webp?du=3000&sqp=CIzntKoG&rs=AOn4CLAyzrUoEy_n5NhZqHhGguLFovpdyA',
+    alt: 'it\'s a really strong prescription video thumbnail'
+}, {
+    name: 'Do you know where the nearest...',
+    artist: 'Chiefofbricks',
+    description: 'Utter irresponsibility on the road',
+    url: 'https://i.ytimg.com/an_webp/T91yNUkp9Us/mqdefault_6s.webp?du=3000&sqp=CPuMtaoG&rs=AOn4CLAVaain64LY1xOfCcunPJdFBHi9VA',
+    alt: 'Do you know where the nearest... video thumbnail'
+}, {
+    name: 'You said that wrong',
+    artist: 'Chiefofbricks',
+    description: 'This one is pacifically about saying stuff wrong!',
+    url: 'https://i.ytimg.com/an_webp/mmPOjsWluZo/mqdefault_6s.webp?du=3000&sqp=COqJtaoG&rs=AOn4CLBWx87F1KmTETv8jA6u0huybLJdrg',
+    alt: 'You said that wrong video thumbnail'
+}, {
+    name: 'We came up with an original idea',
+    artist: 'Chiefofbricks',
+    description: 'We did it! After days of soul-searching and scouring every creative vessel we could find, we actually accomplished it... we came up with an original idea!',
+    url: 'https://i.ytimg.com/an_webp/G2B5pq84mrI/mqdefault_6s.webp?du=3000&sqp=CIqPtaoG&rs=AOn4CLDkKJK7C6o18Kjb9xNeoiNkYRigYA',
+    alt: 'We came up with an original idea video thumbnail'
+}, {
+    name: 'An Honest Interview',
+    artist: 'Chiefofbricks',
+    description: 'One of my weaknesses is that I can be honest to a fault!',
+    url: 'https://i.ytimg.com/an_webp/op--C8oQzfI/mqdefault_6s.webp?du=3000&sqp=CID5tKoG&rs=AOn4CLCDUn2ajg-hSLrzBOYC47-b3jMFUw',
+    alt: 'An Honest Interview video thumbnail'
+}, {
+    name: 'Can these guys survive the apocalypse?',
+    artist: 'Chiefofbricks',
+    description: "There's no tv, there's no radio, there's not even any internet! Can the apocalypse get any more boring? These misfits travel the land until they can find a way to stop it!",
+    url: 'https://i.ytimg.com/an_webp/tKsCLbme8bU/mqdefault_6s.webp?du=3000&sqp=CIzvtKoG&rs=AOn4CLCCFh47wR_PYXT0wQnJwonWfE7a2A',
+    alt: 'Can these guys survive the apocalypse? video thumbnail'
+}, {
+    name: 'The Intervention',
+    artist: 'Chiefofbricks',
+    description: "The lore deepens, a new chapter begins...",
+    url: 'https://i.ytimg.com/an_webp/wbjFaPs6tkw/mqdefault_6s.webp?du=3000&sqp=COCAtaoG&rs=AOn4CLAkLW4A_E9q7iWkMjGa7bDJAzWK6w',
+    alt: 'The Intervention video thumbnail'
+
+}];
