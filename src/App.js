5b27a45e831e34f8691fd7b5f67eb0cd1c5f0c71
tylerakam <tza13@psu.edu>
2023-12-04 15:53:20 -0500

    updating with my backups

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 // function App() {
@@ -21,78 +21,75 @@ import './App.css';
 //     </div>
 //   );
 // }
-import { useState } from 'react';
-import { thumbList } from './data.js';
-import React, { Component } from 'react';
+// import { useState } from 'react';
+// import { thumbList } from './data.js';
+import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
-import Home from './components/Home';
-import About from './components/About';
-import Contact from './components/Contact';
-import Error from './components/Error';
-import Navigation from './components/Navigation';
+import Navbar from "./components/Navbar/NavbarElements";
 
-class App extends Component {
-    render() {
-        return (
-            <BrowserRouter>
-                <div>
-                    <Navigation />
-                    <Routes>
-                        <Route path="./app.js"/>
-                        <Route path="/about" component={About}/>
-                        <Route path="/contact" component={Contact}/>
-                        <Route component={Error}/>
-                    </Routes>
-                </div>
-            </BrowserRouter>
-        );
-    }
+import Home from "./pages";
+import Gallery from "./pages/gallery";
+import Projects from "./pages/projects";
+import Resume from "./pages/resume";
+
+function App() {
+    return (
+        <BrowserRouter>
+            <Navbar />
+            <Routes>
+                <Route exact path="/" element={<Home />} />
+                <Route path="/gallery" element={<Gallery />} />
+                <Route path="/projects" element={<Projects />} />
+                <Route path="/resume" element={<Resume />} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
 
-export function Gallery() {
-    const [index, setIndex] = useState(0);
-    const [showMore, setShowMore] = useState(false);
-    const hasNext = index < thumbList.length - 1;
-
-    function handleNextClick() {
-        if (hasNext) {
-            setIndex(index + 1);
-        } else {
-            setIndex(0);
-        }
-    }
-
-    function handleMoreClick() {
-        setShowMore(!showMore);
-    }
-
-    let thumb = thumbList[index];
-    return (
-        <>
-            <h1>Recent Chiefofbricks Video Gallery</h1>
-            <button onClick={handleNextClick}>
-                Next
-            </button>
-            <h2>
-                <i>{thumb.name} </i>
-                by {thumb.artist}
-            </h2>
-            <h3>
-                ({index + 1} of {thumbList.length})
-            </h3>
-            <button onClick={handleMoreClick}>
-                {showMore ? 'Hide' : 'Show'} details
-            </button>
-            {showMore && <p>{thumb.description}</p>}
-            <iframe width="560" height="315" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen="true"
-                src={thumb.url}
-                alt={thumb.alt}
-            />
-        </>
-    );
-}
-
-
+// export function Gallery() {
+//     const [index, setIndex] = useState(0);
+//     const [showMore, setShowMore] = useState(false);
+//     const hasNext = index < thumbList.length - 1;
+//
+//     function handleNextClick() {
+//         if (hasNext) {
+//             setIndex(index + 1);
+//         } else {
+//             setIndex(0);
+//         }
+//     }
+//
+//     function handleMoreClick() {
+//         setShowMore(!showMore);
+//     }
+//
+//     let thumb = thumbList[index];
+//     return (
+//         <>
+//             <h1>Recent Chiefofbricks Video Gallery</h1>
+//             <button onClick={handleNextClick}>
+//                 Next
+//             </button>
+//             <h2>
+//                 <i>{thumb.name} </i>
+//                 by {thumb.artist}
+//             </h2>
+//             <h3>
+//                 ({index + 1} of {thumbList.length})
+//             </h3>
+//             <button onClick={handleMoreClick}>
+//                 {showMore ? 'Hide' : 'Show'} details
+//             </button>
+//             {showMore && <p>{thumb.description}</p>}
+//             <iframe width="560" height="315" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen="true"
+//                 src={thumb.url}
+//                 alt={thumb.alt}
+//             />
+//         </>
+//     );
+// }
+//
+//
